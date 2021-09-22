23bfbbda443f39510b24acf6e0085233fb613207
wdiepeveen <willem_diep@hotmail.com>
2021-09-22 17:48:32 +0100

    name change so3 mesh refinement

--- projects/rkhs_lifting/src/integrators/base/refined_sd.py
+++ projects/rkhs_lifting/src/integrators/base/refined_sd.py
@@ -2,7 +2,7 @@ import numpy as np
 import os
 
 from projects.rkhs_lifting.src.integrators.base import SO3_Integrator
-from projects.rkhs_lifting.src.mesh.so3_midpoint_refinement import SO3_Midpoint_Refinement
+from projects.rkhs_lifting.src.mesh.midpoint_refinement import Midpoint_Refinement
 
 
 class Refined_SD(SO3_Integrator):
@@ -15,7 +15,7 @@ class Refined_SD(SO3_Integrator):
                  ):
         assert issubclass(type(base_integrator), SO3_Integrator)
 
-        refiner = SO3_Midpoint_Refinement(quats=base_integrator.quaternions, h=resolution)
+        refiner = Midpoint_Refinement(quats=base_integrator.quaternions, h=resolution)
         # refined_integrator = SO3_Integrator(refiner.verts, dtype=dtype)
 
         super().__init__(refiner.verts, dtype=dtype)
