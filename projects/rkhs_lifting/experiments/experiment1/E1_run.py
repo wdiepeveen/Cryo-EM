ee652e9f9bd0a6c1685b5a69bf357efd66b106f4
wdiepeveen <willem_diep@hotmail.com>
2021-09-27 12:00:36 +0100

    Update E1_run.py

--- projects/rkhs_lifting/experiments/experiment1/E1_run.py
+++ projects/rkhs_lifting/experiments/experiment1/E1_run.py
@@ -90,7 +90,7 @@ def run_experiment(exp=None,
     base_integrator = SD1821()
     resolution = np.pi / 10
     refined_integrator = Refined_SD(base_integrator=base_integrator, resolution=resolution, dtype=dtype)
-    radius = 2 * resolution
+    radius = 1.5 * resolution
     kernel = Rescaled_Cosine_Kernel(quaternions=refined_integrator.quaternions, radius=radius, dtype=dtype)
 
     rkhs_integrator = RKHS_Density_Integrator(base_integrator=refined_integrator, kernel=kernel, dtype=dtype)
