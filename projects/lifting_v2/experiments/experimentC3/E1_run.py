different rescaling makes new approach work - Still bug in scaling though

projects/lifting_v2/experiments/experimentC3/E1_run.py
@@ -25,6 +25,8 @@ def run_experiment(exp=None,
                    snr=1.,
                    img_size=65,
                    mr_repeat=1,
+                   tau1=1,
+                   tau2=None,
                    J0 = 10,
                    rots_reg_scaling_param=66 / 100,  # eta
                    data_path=None,
@@ -102,15 +104,16 @@ def run_experiment(exp=None,
     # Estimate sigma
     squared_noise_level = 1 / (1 + snr) * np.sum(np.var(sim.images(0, np.inf).asnumpy(), axis=(1, 2)))
     print("sigma = {}".format(squared_noise_level))
-    tau = np.sum(exp_vol_gt.asnumpy() ** 2)
-
-    print("tau = {}".format(tau))
+    # tau = np.sum(exp_vol_gt.asnumpy() ** 2)
+    #
+    # print("tau = {}".format(tau))
 
     integrator = SD1821MRx(repeat=mr_repeat, dtype=dtype)
 
     solver = Lifting_Solver3(vol=vol_init,
                              squared_noise_level=squared_noise_level,
-                             volume_reg_param=tau,
+                             volume_reg_param=tau1,
+                             volume_kernel_reg_param=tau2,
                              images=sim.images(0, np.inf),
                              filter=sim.unique_filters[0],
                              integrator=integrator,

projects/lifting_v2/src/plans/lifting_plan3.py
@@ -4,9 +4,10 @@ import logging
 import quaternionic
 
 from scipy.spatial.transform import Rotation as R
+from scipy.fft import ifftshift
 
 from aspire.image import Image
-from aspire.utils.coor_trans import grid_2d
+from aspire.utils.coor_trans import grid_2d, grid_3d
 from aspire.volume import Volume
 
 from projects.lifting_v2.src.plans import Plan
@@ -23,6 +24,7 @@ class Lifting_Plan3(Plan):
                  rots_coeffs=None,
                  squared_noise_level=None,  # sigma
                  volume_reg_param=None,  # tau
+                 volume_kernel_reg_param=None,  # tau2
                  # data
                  images=None,  # f_i
                  # parameters
@@ -91,8 +93,11 @@ class Lifting_Plan3(Plan):
         self.vol = vol
         self.rots_coeffs = rots_coeffs
         self._points = None
+
         self.sigmas = squared_noise_level * np.ones((self.N,))
         self.tau = volume_reg_param
+        self.tau2 = volume_kernel_reg_param
+        self.vol_reg_kernel = self.construct_vol_reg_kernel()
 
         self.theta = vol_underrelaxation
 
@@ -232,12 +237,10 @@ class Lifting_Plan3(Plan):
 
         return h
 
-    def construct_reg_kernel(self):
-        k=2
-        # use grid_3d from aspire.utils.coord_trans - also see aspire.volume.rotated_grids
-        # multiply by pi
-        # Compute function on these indices
-        # TODO checkout how we get (2L)**3 sized matrix from this
-        #  - see anufft for this?
-        #  - checkout documentation on FFT
-        # TODO We only have to compute this once and then save it -> then just add it in the solver
+    def construct_vol_reg_kernel(self):
+        grid3d = grid_3d(2 * self.L, shifted=True)
+        x = np.pi * grid3d["x"]
+        y = np.pi * grid3d["y"]
+        z = np.pi * grid3d["z"]
+        return ifftshift(np.sqrt(x ** 2 + y ** 2 + z ** 2).astype(self.dtype), axes=(0, 1, 2))
+        # return np.sqrt(x ** 2 + y ** 2 + z ** 2).astype(self.dtype)

projects/lifting_v2/src/solvers/lifting_solver3.py
@@ -23,6 +23,7 @@ class Lifting_Solver3(Joint_Volume_Rots_Solver):
                  vol=None,
                  squared_noise_level=None,  # sigma
                  volume_reg_param=None,  # tau
+                 volume_kernel_reg_param=None,  # tau2
                  # data
                  images=None,  # f_i
                  # parameters
@@ -42,7 +43,8 @@ class Lifting_Solver3(Joint_Volume_Rots_Solver):
                  ):
         plan = Lifting_Plan3(vol=vol,
                              squared_noise_level=squared_noise_level,  # sigma
-                             volume_reg_param=volume_reg_param,  # tau
+                             volume_reg_param=volume_reg_param,  # tau1
+                             volume_kernel_reg_param=volume_kernel_reg_param,  # tau2
                              images=images,
                              filter=filter,
                              amplitude=amplitude,
@@ -221,10 +223,14 @@ class Lifting_Solver3(Joint_Volume_Rots_Solver):
         kernel_f = fft2(kernel, axes=(0, 1, 2))
         kernel_f = np.real(kernel_f)
 
+        if self.plan.tau2 is not None:
+            logger.info("Add ramp filter regularisation")
+            # kernel_f += 1 / (L ** 6) * self.plan.tau / self.plan.tau2 * self.plan.vol_reg_kernel ** 20  # TODO get rid of 1/L6
+            kernel_f += self.plan.tau / self.plan.tau2 * self.plan.vol_reg_kernel ** 2
+            # TODO check what happens if we have the zero frequency in the top corner (so also no shift in constructor)
+
         f_kernel = FourierKernel(kernel_f, centered=False)
         f_kernel += 1
-
-        # TODO add second regularisaiton term
 
         f_kernel = FourierKernel(
             1.0 / f_kernel.kernel, centered=False
@@ -232,7 +238,8 @@ class Lifting_Solver3(Joint_Volume_Rots_Solver):
 
         # apply kernel
         vol = np.real(f_kernel.convolve_volume(src.T)
-                      / (L ** 3)  # Compensation for the lack of scaling in the forward operator
+                      / (L ** 2)  # Compensation for the lack of scaling in the forward operator (according to ASPIRE)
+                      # / (L ** 3)  # Compensation for the lack of scaling in the forward operator
                       ).astype(dtype)
 
         self.plan.vol = Volume((1 - self.plan.theta) * vol + self.plan.theta * self.plan.vol.asnumpy()[0])